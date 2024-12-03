29ee628112c0943c7660a1258b36d41f95e3f2c9
intel <93615583+intel1337@users.noreply.github.com>
2024-12-03 16:21:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,18 +11,18 @@ def main():
     print("Current minute ", myobj.minute)
     url = input("Enter WebHook URL : ")
     exos = input("Enter Exos Content : ")
-
-    if myobj.hour != 9 and myobj.minute != 0: # Change to == instead of != for the code to work as a service and not a test version
-        app_message = {"text": "Salut c'est le bot des exos du matin ! Voici un exo au hasard : " + exos}
-        message_headers = {"Content-Type": "application/json; charset=UTF-8"}
-        http_obj = Http()
-        response = http_obj.request(
-            uri=url,
-            method="POST",
-            headers=message_headers,
-            body=dumps(app_message),
-        )
-        print(response)
+    while True:
+        if myobj.hour == 9 and myobj.minute == 0: # Change to == instead of != for the code to work as a service and not a test version
+            app_message = {"text": "Salut c'est le bot des exos du matin ! Voici un exo au hasard : " + exos}
+            message_headers = {"Content-Type": "application/json; charset=UTF-8"}
+            http_obj = Http()
+            response = http_obj.request(
+                uri=url,
+                method="POST",
+                headers=message_headers,
+                body=dumps(app_message),
+            )
+            print(response)
 
 if __name__ == "__main__":
     main()
